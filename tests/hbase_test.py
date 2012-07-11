2d80b6275052b52944118f0245922fb0fd5ac4ca
Brandyn A. White <bwhite@dappervision.com>
2012-07-10 20:26:00 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- tests/hbase_test.py
+++ tests/hbase_test.py
@@ -8,12 +8,15 @@ st = time.time()
 
 out = 'out-%f/0' % st
 hadoopy_hbase.launch_frozen('testtable', out, 'hbase_test_job.py', columns=['colfam1:'])
-print list(hadoopy.readtb(out)[:2])
+results = hadoopy.readtb(out)
+print([results.next() for x in range(2)])
 
 out = 'out-%f/1' % st
 hadoopy_hbase.launch_frozen('testtable', out, 'hbase_test_job.py')
-print list(hadoopy.readtb(out)[:2])
+results = hadoopy.readtb(out)
+print([results.next() for x in range(2)])
 
 out = 'out-%f/2' % st
 hadoopy_hbase.launch_frozen('testtable', out, 'hbase_test_job2.py')
-print list(hadoopy.readtb(out)[:2])
+results = hadoopy.readtb(out)
+print([results.next() for x in range(2)])
