471747fcf69343379f7491e214cec35a3dab4e62
Brandyn A. White <bwhite@dappervision.com>
2012-07-10 20:23:55 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- tests/hbase_test.py
+++ tests/hbase_test.py
@@ -4,7 +4,16 @@ import time
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-out = 'out-%f' % time.time()
-hadoopy_hbase.launch_frozen('testtable', out, 'hbase_test_job.py', columns=['colfam1:'])
+st = time.time()
 
-print list(hadoopy.readtb(out))
+out = 'out-%f/0' % st
+hadoopy_hbase.launch_frozen('testtable', out, 'hbase_test_job.py', columns=['colfam1:'])
+print list(hadoopy.readtb(out)[:2])
+
+out = 'out-%f/1' % st
+hadoopy_hbase.launch_frozen('testtable', out, 'hbase_test_job.py')
+print list(hadoopy.readtb(out)[:2])
+
+out = 'out-%f/2' % st
+hadoopy_hbase.launch_frozen('testtable', out, 'hbase_test_job2.py')
+print list(hadoopy.readtb(out)[:2])
