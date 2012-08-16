86b930b3be7e0795d850df061d03007ed3530621
Brandyn A. White <bwhite@dappervision.com>
2012-08-16 18:05:49 -0400

    Updated tests and added launch
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- hadoopy_hbase/__init__.py
+++ hadoopy_hbase/__init__.py
@@ -10,3 +10,14 @@ def launch_frozen(in_name, out_name, script_path, hbase_in=True, hbase_out=False
     jobconfs.append('hbase.mapred.tablecolumns="%s"' % ' '.join(columns))
     kw['jobconfs'] = jobconfs
     hadoopy.launch_frozen(in_name, out_name, script_path, **kw)
+
+
+def launch(in_name, out_name, script_path, hbase_in=True, hbase_out=False, columns=(), **kw):
+    if hbase_in:
+        kw['input_format'] = 'com.dappervision.hbase.mapred.TypedBytesTableInputFormat'
+    if hbase_out:
+        kw['output_format'] = 'com.dappervision.hbase.mapred.TypedBytesTableOutputFormat'
+    jobconfs = kw.get('jobconfs', [])
+    jobconfs.append('hbase.mapred.tablecolumns="%s"' % ' '.join(columns))
+    kw['jobconfs'] = jobconfs
+    hadoopy.launch(in_name, out_name, script_path, **kw)
--- tests/hbase_test.py
+++ tests/hbase_test.py
@@ -6,12 +6,14 @@ logging.basicConfig(level=logging.DEBUG)
 
 st = time.time()
 
+# NOTE(brandyn): If launch fails, you may need to use launch_frozen see hadoopy.com for details
+
 out = 'out-%f/0' % st
-hadoopy_hbase.launch_frozen('testtable', out, 'hbase_test_job.py', columns=['colfam1:'])
+hadoopy_hbase.launch('testtable', out, 'hbase_test_job.py', columns=['colfam1:'])
 results = hadoopy.readtb(out)
 print([results.next() for x in range(2)])
 
 out = 'out-%f/1' % st
-hadoopy_hbase.launch_frozen('testtable', out, 'hbase_test_job2.py', columns=['colfam1:'])
+hadoopy_hbase.launch('testtable', out, 'hbase_test_job2.py', columns=['colfam1:'])
 results = hadoopy.readtb(out)
 print([results.next() for x in range(2)])
