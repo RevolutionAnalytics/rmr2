fb94c963c0131a01a89efd3abd57a611db965d84
Antonio Piccolboni <antonio@piccolboni.info>
2013-02-07 17:57:31 -0800

    hbase input examples

---
+++ pkg/examples/hbase.R
@@ -0,0 +1,38 @@
+from.dfs(
+  mapreduce(
+    input="blogposts", 
+    input.format = 
+      make.input.format(
+        "hbase", 
+        family.columns = 
+          list(
+            image= list("bodyimage"), 
+            post = list("author", "body")), 
+        key.deserialize = "raw", 
+        cell.deserialize = "raw", 
+        dense = T, 
+        atomic = T), 
+    backend.parameters = 
+      list(
+        hadoop = list(libjars = "../hadoopy_hbase.jar"))))
+
+freebase.input.format = 
+  make.input.format(
+    "hbase", 
+    family.columns = 
+      list(
+        name = "", 
+        `freebase-types` = ""), 
+    key.deserialize = "raw", 
+    cell.deserialize = "raw", 
+    dense = T, 
+    atomic = T)
+
+from.dfs(
+  mapreduce(
+    input = "freebase",
+    input.format = freebase.input.format,
+    backend.parameters = 
+      list(
+        hadoop = list(libjars = "../hadoopy_hbase.jar")),
+    map = function(k,v) keyval(k[1,], v[1,])))
