578f70fa6922b63a6a2e682a4a32e87708f8eeb1
Antonio Piccolboni <antonio@piccolboni.info>
2013-02-19 12:18:07 -0800

    copyrights

--- pkg/examples/large-kmeans-test.R
+++ pkg/examples/large-kmeans-test.R
@@ -1,3 +1,17 @@
+# Copyright 2013 Revolution Analytics
+#    
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 input.1000 = mapreduce (input = to.dfs(1:1000), 
                         map = function(k, v) keyval(rnorm(1), v), 
                         reduce = to.reduce(identity))
