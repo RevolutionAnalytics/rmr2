093196e5d5765aa955c3edc3b7cbd2977cfe108a
Antonio Piccolboni <antonio@piccolboni.info>
2013-05-17 20:40:36 -0700

    bugs, perf improvements

--- pkg/examples/ngram.R
+++ pkg/examples/ngram.R
@@ -17,6 +17,7 @@
 # $WHIRR_HOME/bin/whirr  run-script --script ~/Projects/Revolution/RHadoop/rmr/pkg/tools/whirr/rmr-1.3.sh  --config ~/Projects/Revolution/RHadoop/rmr/pkg/tools/whirr/hadoop-ec2-lzo.properties
 # $WHIRR_HOME/bin/whirr  run-script --script ~/Projects/Revolution/RHadoop/rmr/pkg/tools/whirr/lzo.sh  --config ~/Projects/Revolution/RHadoop/rmr/pkg/tools/whirr/hadoop-ec2-lzo.properties
 
+## @knitr load.rmr2
 library(rmr2)
 ## @knitr fake-data
 fake.size = 2000000
@@ -35,11 +36,12 @@ writeLines(
 source = "/tmp/fake-ngram-data"
 # rmr.options(backend = "local")
 
+#Timing for 12 + 1 node EC2 cluster m1.large instances
 ## @knitr distcp
 # hadoop distcp s3n://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@datasets.elasticmapreduce/ngrams/books/20090715/eng-all/1gram/ hdfs:///user/antonio/
 ## @knitr scatter
-source = scatter("/user/antonio/1gram/data")
-
+## source = scatter("/user/antonio/1gram/data")
+# 33 mins
 ## @knitr ngram.format
 ngram.format = function(lines){
   data = 
@@ -56,24 +58,28 @@ filter.map = function(., lines) {
   ngram.data = ngram.format(lines)
   ngram.data[
     regexpr(
-      "^[A-za-z+'-]+$", 
+      "^[A-Za-z]+$", 
       ngram.data$ngram) > -1 & 
       ngram.data$year > 1800,]}
 
 ## @knitr filtered.data
+source = "/user/antonio/1gram/data"
 rmr.options(keyval.length = 10^5)
 filtered.data = 
   mapreduce(input = source,
-            input.format = "text", #comment this on real data
+           # input.format = "text", #comment this on real data
             map = filter.map)
+## @knitr end
+#20 mins, 
 ## @knitr sample-data
 from.dfs(rmr.sample(filtered.data, method="any", n = 50))
-
+## @knitr end
+#5 mins
 
 ## @knitr totals.map
 totals.map = 
   function(., ngram.data) {
-    total = tapply(ngram.data$count, ngram.data$year, sum)
+    total = tapply(as.numeric(ngram.data$count), ngram.data$year, sum, na.rm = TRUE)
     keyval(names(total), as.vector(total))}
 
 ## @knitr totals.reduce
@@ -88,16 +94,17 @@ year.totals.kv =
               map = totals.map,
               reduce = totals.reduce,
               combine = TRUE))
+## @knitr end
+#9 mins
 
 ## @knitr year.totals-finish
 year.totals = c()
 year.totals[keys(year.totals.kv)] = values(year.totals.kv)
-
 ## @knitr outlier.map
 library(bitops)
 outlier.map = 
   function(., ngram.data) {
-    k = ngram.data$year + cksum(ngram.data$ngram)%%10/10
+    k = ngram.data$year + cksum(ngram.data$ngram)%%100/100
     c.keyval(
       keyval(k, ngram.data),
       keyval(k + 1, ngram.data))}
@@ -112,29 +119,33 @@ outlier.reduce =
       NULL
     else {
       ngram.data = dcast(ngram.data, ngram ~ year, fill = 0)
-      filter = 
-        !adjOutlyingness(
-          log(
-            t(
-              t(ngram.data[,2:3] + 1)/
-                as.vector(year.totals[as.character(years)] + 1)))
-          )$nonOut
-      as.character(ngram.data[filter,'ngram'])}}
+      tryCatch({
+        filter = 
+          !adjOutlyingness(
+            log(
+              t(
+                t(ngram.data[,2:3] + 1)/
+                  as.vector(
+                    year.totals[as.character(years)] + 1))),
+            alpha.cutoff = .95)$nonOut
+        as.character(ngram.data[filter,'ngram'])},
+               error = function(e) NULL)}}
+## @knitr end
 
+# watch out the next doesn't seem to work beyond 10^5 ngrams
+# problem is inefficient assignment, still investigating
 ## @knitr outlier.ngram
-outlier.ngram = c()
-outlier.ngram[
+outlier.ngram =  
   unique(
     values(
       from.dfs(
         mapreduce(
           input = filtered.data,
           map = outlier.map,
-          reduce = outlier.reduce))))] = TRUE
+          reduce = outlier.reduce))))
 
-## @knitr ngram.filter
-ngram.filter = 
-  function(ngram) !is.na(outlier.ngram[as.character(ngram)])  
+## @knitr end
+# 8 hours
 
 ## @knitr plot.data
 plot.data = 
@@ -142,8 +153,14 @@ plot.data =
     from.dfs(
       mapreduce(
         input = filtered.data,
-        map = function(., ngram.data) 
-          ngram.data[ngram.filter(ngram.data$ngram),])))
+        map = 
+          function(., ngram.data) 
+            ngram.data[
+              is.element(
+                as.character(ngram.data$ngram), 
+                outlier.ngram),])))
+## @knitr end
+# mins
 
 ## @knitr plot.data.frame
 plot.data = 
@@ -153,7 +170,7 @@ plot.data =
     variable.name="year",
     value.name = "count")
 plot.data$freq  = 
-  plot.data$count/
+  (plot.data$count + 0.1)/
   year.totals[as.character(plot.data$year)]
 plot.data = 
   plot.data[order(plot.data$ngram, plot.data$year),]
@@ -171,6 +188,7 @@ plot.data$average =
   sqrt(plot.data$freq*plot.data$freq.prev)
 plot.data$ratio = 
   plot.data$freq/plot.data$freq.prev
+plot.data$time = as.integer(as.character(plot.data$time))
 
 ## @knitr plot
 suppressPackageStartupMessages(library(googleVis))
@@ -178,6 +196,5 @@ motion.chart =
   gvisMotionChart(
     plot.data[,c("id","time","average","ratio")], 
     options = list(height = 1000, width = 2000))
-plot.data$time = as.integer(as.character(plot.data$time))
 plot(motion.chart)
 print(motion.chart, "chart")  
