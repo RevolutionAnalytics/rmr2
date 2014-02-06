30de3f133bd6fcc054f0d8f8143e413e3f3af7a1
Antonio Piccolboni <antonio@piccolboni.info>
2014-02-05 23:37:29 -0800

    connect the pieces

--- pkg/R/IO.R
+++ pkg/R/IO.R
@@ -115,7 +115,7 @@ to.data.frame =
       lapply(
         seq_along(x), 
         function(i)
-          if(is.atomic(template[[i]])) unlist(x[[i]]) else I(x[[i]]))
+          if(is.atomic(template[[i]])) unlist(x[[i]]) else I(splat(c)(x[[i]])))
     names(y) = names(template)
     data.frame(y)}
 
