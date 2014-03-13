e709573ccdee32515c91978724a28b414bb735b7
Antonio Piccolboni <antonio@piccolboni.info>
2014-03-13 11:35:52 -0700

    time to remove warning (added at 1->2 transition)

--- pkg/R/mapreduce.R
+++ pkg/R/mapreduce.R
@@ -208,8 +208,6 @@ to.dfs =
     kv, 
     output = dfs.tempfile(), 
     format = "native") {
-    if(!is.keyval(kv))
-      warning("Converting to.dfs argument to keyval with a NULL key")
     kv = as.keyval(kv)
     tmp = tempfile()
     dfs.output = to.dfs.path(output)
